53d912aa079dae9d150bb1e79d606ab11e775de3
David Elias <david@david-elias.net>
2013-03-05 07:57:32 +0000

    Assign asset to LessCompiler to pass the asset's dirname as a load path

--- staticassets/compilers/less.py
+++ staticassets/compilers/less.py
@@ -5,3 +5,11 @@ class LessCompiler(CommandCompiler):
     content_type = 'text/css'
     command = 'lessc'
     params = ['-']
+
+    def get_args(self):
+        args = super(LessCompiler, self).get_args()
+        return args + ['--include-path=%s' % self.asset.storage.path(self.asset.attributes.dirname)]
+
+    def compile(self, asset):
+        self.asset = asset
+        return super(LessCompiler, self).compile(asset)
