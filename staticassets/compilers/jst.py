add27a1e2f72e2e7824f2cbc8df17ff969659450
David Elias <stout.elias@gmail.com>
2014-10-28 17:10:00 +0000

    Improve jst and ejs template compilers

--- staticassets/compilers/jst.py
+++ staticassets/compilers/jst.py
@@ -1,15 +1,19 @@
+from .. import settings
 from .base import BaseCompiler, CommandCompiler
 
 
 JST_SOURCE = "(function() { %(namespace)s || (%(namespace)s = {}); %(namespace)s['%(name)s'] = %(content)s;}).call(this);"
 
 EJS_COMPILER = """
-var u = require('underscore'), source = '';
+var _ = require('underscore'), source = '';
+_.templateSettings = %(template_settings)s;
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(chunk) { source += chunk; });
-process.stdin.on('end', function() { process.stdout.write(u.template(source).source); });
-""".strip('\n')
+process.stdin.on('end', function() { process.stdout.write(_.template(source).source); });
+""".strip('\n') % {
+    'template_settings': settings.EJS_TEMPLATE_SETTINGS
+}
 
 
 class JstCompiler(BaseCompiler):
@@ -23,7 +27,7 @@ class JstCompiler(BaseCompiler):
         asset.content = self.source % {
             'namespace': self.namespace,
             'name': asset.attributes.path_without_extensions,
-            'content': asset.content
+            'content': asset.content.replace('\n', '')
         }
 
 
