058290674a27d68cc6258fc6c66377bdd57d3fb1
David Elias <stout.elias@gmail.com>
2014-11-26 16:58:58 +0000

    Remove inexistent compiler import

--- staticassets/compilers/__init__.py
+++ staticassets/compilers/__init__.py
@@ -2,6 +2,5 @@ from .base import BaseCompiler, CommandCompiler, get, get_mimetypes  # noqa
 from .coffeescript import CoffeeScriptCompiler as CoffeeScript  # noqa
 from .stylus import StylusCompiler as Stylus  # noqa
 from .sass import SassCompiler as Sass  # noqa
-from .sassc import SassCCompiler as SassC  # noqa
 from .less import LessCompiler as Less  # noqa
 from .jst import JstCompiler as Jst, EjsCompiler as Ejs  # noqa
